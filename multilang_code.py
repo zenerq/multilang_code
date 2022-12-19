e819fbe61265a9dc2b2c8c2525af69fa06038d2e
zenerq <rokosamje@gmail.com>
2022-12-19 19:17:11 +0100

    Add files via upload

---
+++ multilang_code.py
@@ -0,0 +1,66 @@
+import streamlit as st 
+import os
+import openai
+import pybase64
+
+LOGO_IMAGE = "logo.png"
+
+st.markdown(
+    """
+    <style>
+    .container {
+        display: flex;
+    }
+    .logo-text {
+        font-weight:700 !important;
+        font-size:50px !important;
+        color: #ffffff !important;
+        padding-top: 75px !important;
+    }
+    .logo-img {
+        float:right;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    f"""
+    <div class="container">
+        <img class="logo-img" src="data:image/png;base64,{pybase64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+        <p class="logo-text">What are you looking for?</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.header("Code Solution generator in different languages") 
+language_option = st.selectbox(
+    'In which language do you want your code solution?',
+    ('Python', 'Java', 'C++','Javascript','Go','Ruby'))
+question=st.text_area("Type Problem")
+button= st.button("Generate code")
+
+def gen_auto_response(question,language_option):
+
+    openai.api_key = "YOUR_KEY"
+    response = openai.Completion.create(
+        model="code-cushman-001",
+        prompt=f""""Given a {language_option} solution for the code question below 
+                Question: {question} 
+                {language_option} Solution: """,
+        temperature=0,
+        max_tokens=1124,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(response)
+    return response.choices[0].text
+	
+if question and button and language_option:
+    with st.spinner("Generating code, Please Wait"):
+        reply=gen_auto_response(question,language_option)
+        st.code(reply)
